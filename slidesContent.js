1d1867128a8e9621d8e57027f3c54b773fa73a98
nikskr <91470938+nikskr@users.noreply.github.com>
2026-09-24 17:00:49 +0300

    refactor: extract slides content array to separated file

---
+++ slidesContent.js
@@ -0,0 +1,23 @@
+export const slidesContent = [
+  {
+    name: 'S`mores Frappuccino',
+    description:
+      'This new drink takes an espresso and mixes it with brown sugar and cinnamon before being topped with oat milk.',
+    price: '$5.50',
+    img: './assets/images/coffee-slider-1.png',
+  },
+  {
+    name: 'Caramel Macchiato',
+    description:
+      'Fragrant and unique classic espresso with rich caramel-peanut syrup, with cream under whipped thick foam.',
+    price: '$5.00',
+    img: './assets/images/coffee-slider-2.png',
+  },
+  {
+    name: 'Ice coffee',
+    description:
+      'A popular summer drink that tones and invigorates. Prepared from coffee, milk and ice.',
+    price: '$4.50',
+    img: './assets/images/coffee-slider-3.png',
+  },
+];
